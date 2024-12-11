94a3a35870fd569a00ebb147c177df79e2207300
obidjonjoraboyev <obidjonn007@icloud.com>
2024-12-11 17:24:17 +0500

    updated

--- lib/photo_opener.dart
+++ lib/photo_opener.dart
@@ -8,22 +8,30 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
-onOpenPhoto(
-    {required BuildContext context,
-    required List<String> images,
-    Widget? closeButton,
-    String? closeText,
-    Color? backgroundColor,
-    Color? loaderColor}) {
+
+onOpenPhoto({
+  required BuildContext context,
+  required List<String> images,
+  Widget? closeButton,
+  String? closeText,
+  Color? backgroundColor,
+  Color? secondaryColor,
+  Color? loaderColor,
+  double? maxScale,
+  double? minScale,
+  PageController? pageController,
+  ValueChanged<int>? onPageChange,
+  TextStyle? topTextStyle,
+}) {
   double barrierColor = 1;
   bool isOpen = true;
   double height = MediaQuery.sizeOf(context).height;
   double width = MediaQuery.sizeOf(context).width;
   PhotoViewScaleState state = PhotoViewScaleState.initial;
-  final PageController pageController = PageController();
+  final PageController pageCtrl = pageController ?? PageController();
   int currentPage = 1;
   PhotoViewScaleStateController photoController =
-      PhotoViewScaleStateController();
+  PhotoViewScaleStateController();
   isOpen = true;
   currentPage = 1;
   photoController.reset();
@@ -46,9 +54,9 @@ onOpenPhoto(
                 setState(() {});
                 !isOpen
                     ? SystemChrome.setEnabledSystemUIMode(
-                        SystemUiMode.immersive)
+                    SystemUiMode.immersive)
                     : SystemChrome.setEnabledSystemUIMode(
-                        SystemUiMode.edgeToEdge);
+                    SystemUiMode.edgeToEdge);
               },
               child: Stack(
                 alignment: Alignment.topCenter,
@@ -56,7 +64,7 @@ onOpenPhoto(
                   Dismissible(
                     onUpdate: (v) {
                       barrierColor =
-                          (1 - v.progress * 4) >= 0 ? (1 - v.progress * 4) : 0;
+                      (1 - v.progress * 4) >= 0 ? (1 - v.progress * 4) : 0;
                       setState(() {});
                     },
                     movementDuration: const Duration(milliseconds: 500),
@@ -68,33 +76,38 @@ onOpenPhoto(
                       Navigator.pop(context);
                     },
                     direction: state == PhotoViewScaleState.initial ||
-                            state == PhotoViewScaleState.originalSize ||
-                            state == PhotoViewScaleState.covering
+                        state == PhotoViewScaleState.originalSize ||
+                        state == PhotoViewScaleState.covering
                         ? DismissDirection.vertical
                         : DismissDirection.none,
                     key: const Key("value"),
                     child: SizedBox(
                       height: height,
                       child: PhotoViewGallery.builder(
-                        pageController: pageController,
+                        pageController: pageCtrl,
                         onPageChanged: (v) {
                           currentPage = v + 1;
                           setState(() {});
                           photoController.reset();
+
+                          onPageChange?.call(v);
                         },
                         scaleStateChangedCallback: (v) {
                           state = v;
                           setState(() {});
                         },
                         allowImplicitScrolling: true,
-                        scrollPhysics: const BouncingScrollPhysics(),
+                        scrollPhysics: const BouncingScrollPhysics(
+                            parent: AlwaysScrollableScrollPhysics()),
                         builder: (BuildContext context, int index) {
                           return PhotoViewGalleryPageOptions(
                             scaleStateController: photoController,
                             imageProvider: NetworkImage(images[index]),
-                            maxScale: PhotoViewComputedScale.contained * 5,
+                            maxScale: PhotoViewComputedScale.contained *
+                                (maxScale ?? 5),
                             initialScale: PhotoViewComputedScale.contained * 1,
-                            minScale: PhotoViewComputedScale.contained * 1,
+                            minScale: PhotoViewComputedScale.contained *
+                                (minScale ?? 1),
                           );
                         },
                         itemCount: images.length,
@@ -107,12 +120,12 @@ onOpenPhoto(
                               value: event == null
                                   ? 0
                                   : event.cumulativeBytesLoaded /
-                                      event.expectedTotalBytes!.toInt(),
+                                  event.expectedTotalBytes!.toInt(),
                             ),
                           ),
                         ),
                         backgroundDecoration:
-                            const BoxDecoration(color: Colors.transparent),
+                        const BoxDecoration(color: Colors.transparent),
                       ),
                     ),
                   ),
@@ -126,45 +139,78 @@ onOpenPhoto(
                     },
                     child: isOpen
                         ? Opacity(
-                            opacity: barrierColor,
-                            child: Material(
-                              color: Colors.transparent,
-                              child: Container(
-                                color: Colors.black.withOpacity(
-                                    barrierColor == 1 ? 0.5 : barrierColor / 2),
-                                padding: EdgeInsets.only(
-                                    top: MediaQuery.of(context).padding.top +
-                                        (Platform.isAndroid ? 10.h : 0),
-                                    left: 21.w,
-                                    right: 21.w,
-                                    bottom: 5.h),
-                                child: closeButton ??
-                                    Row(
+                      opacity: barrierColor,
+                      child: Material(
+                        color: Colors.transparent,
+                        child: Container(
+                          color: (secondaryColor ?? Colors.black)
+                              .withOpacity(barrierColor == 1
+                              ? 0.5
+                              : barrierColor / 2),
+                          padding: EdgeInsets.only(
+                              top: MediaQuery.of(context).padding.top +
+                                  (Platform.isAndroid ? 10.h : 0),
+                              left: 21.w,
+                              right: 21.w,
+                              bottom: 5.h),
+                          child: closeButton ??
+                              Row(
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Row(
                                       children: [
-                                        GestureDetector(
-                                          onTap: () {
-                                            Navigator.pop(context);
-                                          },
-                                          child: Row(
-                                            children: [
-                                              const Icon(CupertinoIcons.back),
-                                              Text(
-                                                closeText!,
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .titleMedium,
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                        const Spacer(flex: 1),
-                                        Text("$currentPage/3"),
-                                        const Spacer(flex: 2),
+                                        const Icon(CupertinoIcons.back),
+                                        Text(
+                                          closeText ?? "Back",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleMedium,
+                                        )
                                       ],
                                     ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          )
+                        ),
+                      ),
+                    )
+                        : const SizedBox(),
+                  ),
+                  AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 100),
+                    transitionBuilder: (child, animation) {
+                      return FadeTransition(
+                        opacity: animation,
+                        child: child,
+                      );
+                    },
+                    child: isOpen
+                        ? Opacity(
+                      opacity: barrierColor,
+                      child: Material(
+                        color: Colors.transparent,
+                        child: Container(
+                          padding: EdgeInsets.only(
+                              top: MediaQuery.of(context).padding.top +
+                                  (Platform.isAndroid ? 10.h : 0),
+                              left: 21.w,
+                              right: 21.w,
+                              bottom: 5.h),
+                          child: Text(
+                            "$currentPage/${images.length}",
+                            style: topTextStyle ??
+                                TextStyle(
+                                  color: CupertinoColors.white,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.sp,
+                                ),
+                          ),
+                        ),
+                      ),
+                    )
                         : const SizedBox(),
                   ),
                   Positioned(
@@ -179,68 +225,67 @@ onOpenPhoto(
                       },
                       child: isOpen
                           ? Opacity(
-                              opacity: barrierColor,
-                              child: Material(
-                                color: Colors.transparent,
-                                child: Container(
-                                  width: width,
-                                  color: Colors.black.withOpacity(
-                                      barrierColor == 1
-                                          ? 0.5
-                                          : barrierColor / 2),
-                                  padding: EdgeInsets.only(
-                                      bottom: MediaQuery.of(context)
-                                              .padding
-                                              .bottom +
-                                          (Platform.isAndroid ? 10.h : 0),
-                                      left: 21.w,
-                                      right: 21.w,
-                                      top: 8.h),
-                                  child: SingleChildScrollView(
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        ...List.generate(images.length,
-                                            (index) {
-                                          return Padding(
-                                            padding: EdgeInsets.symmetric(
-                                              horizontal: 2.w,
-                                            ),
-                                            child: GestureDetector(
-                                              onTap: () {
-                                                pageController.animateToPage(
-                                                    index,
-                                                    duration: const Duration(
-                                                        milliseconds: 300),
-                                                    curve: Curves.easeInOut);
-                                              },
-                                              child: ClipRRect(
-                                                borderRadius:
-                                                    BorderRadius.circular(4.r),
-                                                child: AnimatedContainer(
+                        opacity: barrierColor,
+                        child: Material(
+                          color: Colors.transparent,
+                          child: Container(
+                            width: width,
+                            color: (secondaryColor ?? Colors.black)
+                                .withOpacity(barrierColor == 1
+                                ? 0.5
+                                : barrierColor / 2),
+                            padding: EdgeInsets.only(
+                                bottom: MediaQuery.of(context)
+                                    .padding
+                                    .bottom +
+                                    (Platform.isAndroid ? 10.h : 0),
+                                left: 21.w,
+                                right: 21.w,
+                                top: 8.h),
+                            child: SingleChildScrollView(
+                              child: Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.center,
+                                children: [
+                                  ...List.generate(images.length,
+                                          (index) {
+                                        return Padding(
+                                          padding: EdgeInsets.symmetric(
+                                            horizontal: 2.w,
+                                          ),
+                                          child: GestureDetector(
+                                            onTap: () {
+                                              pageCtrl.animateToPage(index,
                                                   duration: const Duration(
-                                                      milliseconds: 200),
-                                                  width: 35.sp +
-                                                      (index == currentPage - 1
-                                                          ? 12.w
-                                                          : 0),
-                                                  child: CachedNetworkImage(
-                                                    imageUrl: images[index],
-                                                    height: 45.sp,
-                                                    fit: BoxFit.cover,
-                                                  ),
+                                                      milliseconds: 300),
+                                                  curve: Curves.easeInOut);
+                                            },
+                                            child: ClipRRect(
+                                              borderRadius:
+                                              BorderRadius.circular(4.r),
+                                              child: AnimatedContainer(
+                                                duration: const Duration(
+                                                    milliseconds: 200),
+                                                width: 35.sp +
+                                                    (index == currentPage - 1
+                                                        ? 12.w
+                                                        : 0),
+                                                child: CachedNetworkImage(
+                                                  imageUrl: images[index],
+                                                  height: 45.sp,
+                                                  fit: BoxFit.cover,
                                                 ),
                                               ),
                                             ),
-                                          );
-                                        })
-                                      ],
-                                    ),
-                                  ),
-                                ),
+                                          ),
+                                        );
+                                      })
+                                ],
                               ),
-                            )
+                            ),
+                          ),
+                        ),
+                      )
                           : const SizedBox(),
                     ),
                   ),
@@ -251,3 +296,4 @@ onOpenPhoto(
         });
       });
 }
+
